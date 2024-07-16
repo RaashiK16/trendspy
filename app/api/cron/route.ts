9491c549606a2f4e1b90943840135ade5b8a1e72
RaashiK16 <126188705+RaashiK16@users.noreply.github.com>
2024-07-16 23:40:52 +0530

    commit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -54,7 +54,7 @@ export async function GET() {
                     const emailContent = await generateEmailBody(productInfo, emailNotifType);
                     const userEmails = updatedProduct.users.map((user:any)=>user.email)
 
-                    sendEmail(emailContent,userEmails)
+                    await sendEmail(emailContent,userEmails)
                 }
 
                 return updatedProduct
